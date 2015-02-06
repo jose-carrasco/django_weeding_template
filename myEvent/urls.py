54bb535e64bf1f4fe89629ba10eea9ed48135cc6
Jose Antonio Carrasco <jcarrasco@digitalartsnetwork.com.mx>
2015-02-06 13:30:05 -0600

    Deply static files

--- myEvent/urls.py
+++ myEvent/urls.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import patterns, url
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
 
 from myEvent import views
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
@@ -11,3 +15,8 @@ urlpatterns = patterns('',
     url(r'^confirmacion', views.confirm, name='confirm'),
     url(r'^comparte_tus_fotos', views.photos, name='photos'),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
