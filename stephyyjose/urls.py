3ba70869777af47aa09dae340444334ce9051095
Jose Antonio Carrasco <jcarrasco@digitalartsnetwork.com.mx>
2015-02-06 13:43:55 -0600

    Static files admin

--- stephyyjose/urls.py
+++ stephyyjose/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,3 +11,8 @@ urlpatterns = patterns('',
     url(r'^', include('myEvent.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
